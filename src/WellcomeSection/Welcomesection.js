5c4300f3d0b81701fad50ed91408e876ec9e54a7
Andrew Verbitsky <averbitskiy5@gmail.com>
2021-04-22 22:44:49 +0300

    commit

--- src/WellcomeSection/Welcomesection.js
+++ src/WellcomeSection/Welcomesection.js
@@ -11,22 +11,26 @@ function WelcomeSection() {
           <h1>Wellcome to CETREBA</h1>
           <div className="slide">
             <div className="slide-item item-1">
-              <span className="line"></span>
+              <div className="line-1"></div>
+              <div className="line-2"></div>
               <span className="number">01</span>
               <p>About</p>
             </div>
             <div className="slide-item item-2">
-              <span className="line"></span>
+              <div className="line-1"></div>
+              <div className="line-2"></div>
               <span className="number">02</span>
               <p>Products</p>
             </div>
             <div className="slide-item item-3">
-              <span className="line"></span>
+              <div className="line-1"></div>
+              <div className="line-2"></div>
               <span className="number">03</span>
               <p>Blog</p>
             </div>
             <div className="slide-item item-4">
-              <span className="line"></span>
+              <div className="line-1"></div>
+              <div className="line-2"></div>
               <span className="number">04</span>
               <p>Contact</p>
             </div>
