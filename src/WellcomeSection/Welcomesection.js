41ccd74c5868b05f159a036be6eaa477383e5d0b
Andrew Verbitsky <averbitskiy5@gmail.com>
2021-04-22 22:10:33 +0300

    commit

--- src/WellcomeSection/Welcomesection.js
+++ src/WellcomeSection/Welcomesection.js
@@ -5,9 +5,35 @@ import './Welcomesection.css';
 function WelcomeSection() {
   return (
     <>
-      <div className="container-xl">
-        <h1>Hello</h1>
-      </div>
+      {/* WellcomeSection */}
+      <section>
+        <div className="container-xl">
+          <h1>Wellcome to CETREBA</h1>
+          <div className="slide">
+            <div className="slide-item item-1">
+              <span className="line"></span>
+              <span className="number">01</span>
+              <p>About</p>
+            </div>
+            <div className="slide-item item-2">
+              <span className="line"></span>
+              <span className="number">02</span>
+              <p>Products</p>
+            </div>
+            <div className="slide-item item-3">
+              <span className="line"></span>
+              <span className="number">03</span>
+              <p>Blog</p>
+            </div>
+            <div className="slide-item item-4">
+              <span className="line"></span>
+              <span className="number">04</span>
+              <p>Contact</p>
+            </div>
+          </div>          
+        </div>
+        
+      </section>
       
       
     </>
